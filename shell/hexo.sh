aae2ca647a291133326dee5ff7161a2d586108d5
cooper-q <19221882+cooper-q@users.noreply.github.com>
2019-10-31 11:10:17 +0800

    feat(开机自启): 增加开机自启脚本

---
+++ shell/hexo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+start(){
+  echo 'start'
+  cd /root/project/deployment/local/remote
+  /root/.nvm/versions/node/v10.15.3/bin/hexo clean
+  /usr/bin/nohup /root/.nvm/versions/node/v10.15.3/bin/hexo s &
+}
+
+stop(){
+    processID=`ps -ef |grep -v grep| grep hexo|awk '{print $2}'`
+    bool=`echo $processID|awk '{print($0~/^[-]?([0-9])+[.]?([0-9])+$/)?"true":"false"}'`
+
+    if [ $bool == "true" ]
+    then
+        kill -9 $processID
+    else
+        echo '服务未启动'
+    fi
+}
+case $1 in
+    start)
+        start
+    ;;
+    stop)
+        stop
+    ;;
+esac
--- shell/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-unset GIT_DIR
-cd /root/project/deployment/local/remote
-git reset --hard
-git pull origin master
-source ~/.zshrc
-hexo clean
-ps -ef | grep "hexo" |grep -v grep|awk '{print $2}'|xargs kill -9
-nohup hexo s &
