ef6fd3426aa8b67221195a5a8ca9e4e8b3372ad9
cooper-q <19221882+cooper-q@users.noreply.github.com>
2020-05-06 17:17:30 +0800

    部署

--- shell/init.sh
+++ shell/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# 删除
+rm -rf /root/project/remote
+rm -rf /root/project/local
 # 1.初始化远程仓库
 mkdir -p /root/project/remote && cd /root/project/remote
 git init --bare
@@ -14,8 +17,9 @@ echo '#!/bin/sh' >> post-update
 echo 'unset GIT_DIR' >> post-update
 echo 'cd /root/project/local/remote' >> post-update
 echo 'git pull origin master' >> post-update
-echo "ps -ef | grep 'hexo' |grep -v grep|awk '{print \$2}'|xargs kill -9" >> post-update
-echo 'nohup hexo s' >> post-update
-echo '部署完成' >> post-update
-echo 'exit' >> post-update
+echo '/bin/sh /root/project/local/remote/shell/reload.sh' >> post-update
+#echo "ps -ef | grep 'hexo' |grep -v grep|awk '{print \$2}'|xargs kill -9" >> post-update
+#echo 'nohup hexo s' >> post-update
+#echo '部署完成' >> post-update
+#echo 'exit' >> post-update
 chmod +x post-update
--- shell/reload.sh
+++ shell/reload.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 # 远端部署
-export PATH='/root/.nvm/versions/node/v10.15.3/bin:/usr/bin:/root/.nvm/versions/node/v12.16.2/bin'
+#export PATH='/root/.nvm/versions/node/v10.15.3/bin:/usr/bin:/root/.nvm/versions/node/v12.16.2/bin'
 unset GIT_DIR
-cd /root/project/hexo/local/remote
+cd /root/project/local/remote
 # npm i
 git reset --hard
 git pull origin master
-# source ~/.zshrc
-hexo clean
 ps -ef | grep hexo |grep -v grep|awk '{print $2}'|xargs kill -9
 nohup hexo s &
 echo '部署完成'
