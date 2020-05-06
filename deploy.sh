77375250cf8904c7ec40686a90686981116946f4
cooper-q <19221882+cooper-q@users.noreply.github.com>
2020-05-06 15:28:34 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 # 添加一个githook 并提交至githook服务器进行部署
 echo ''>>git_hook
 git commit -am '部署'
-echo '开始部署腾讯云服务器'
-git push tx master
-echo '开始部署境外vps'
+#echo '开始部署腾讯云服务器'
+#git push tx master
+#echo '开始部署境外vps'
 git push cc master
-echo '部署结束'
+#echo '部署结束'
 
--- shell/reload.sh
+++ shell/reload.sh
@@ -3,7 +3,7 @@
 export PATH='/root/.nvm/versions/node/v10.15.3/bin:/usr/bin:/root/.nvm/versions/node/v12.16.2/bin'
 unset GIT_DIR
 cd /root/project/hexo/local/remote
-npm i
+# npm i
 git reset --hard
 git pull origin master
 # source ~/.zshrc
