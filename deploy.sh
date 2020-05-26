20662ef654a28fdbd00152aad288dfde92654118
cooper-q <19221882+cooper-q@users.noreply.github.com>
2020-05-26 11:32:07 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ if [[ ! -z "$1" ]]
 then
     echo '参数错误'
     exit 0;
-elif [[ $1 = "tx"  ]]
+elif [[ "$1" = "tx"  ]]
 then
     echo '开始部署腾讯云服务器'
     git push tx master
-elif [ $1 = cc ]
+elif [[ "$1" = "cc" ]]
 then
     echo '开始部署境外vps'
     git push cc master
