cc12448bf9824f81178f0c73102f61f39bdcf5d0
cooper-q <19221882+cooper-q@users.noreply.github.com>
2020-07-21 16:08:51 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 version=`cat ./git_hook|awk -F "[.]" '{print $3}'`
 version=`expr ${version} + 1`
 echo ${version}
-echo 'v0.0.1' > git_hook
+echo v0.0.${version} > git_hook
 git commit -am '部署'
 if [[ -z "$1" ]]
 then
