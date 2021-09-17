65df3bd6c77b4e3f4ba02d5bb2ddbb0bfa0d2765
cooper-q <19221882+cooper-q@users.noreply.github.com>
2021-09-17 15:55:41 +0800

    feat(部署): update

--- shell/reload.sh
+++ shell/reload.sh
@@ -2,8 +2,7 @@
 # 远端部署
 export PATH='/root/.nvm/versions/node/v10.15.3/bin:/usr/bin:/root/.nvm/versions/node/v12.16.2/bin'
 unset GIT_DIR
-cd /root/project/local/remote
-# npm i
+cd /root/project/MattMeng_hexo
 git reset --hard
 git pull origin master
 ps -ef | grep hexo |grep -v grep|awk '{print $2}'|xargs kill -9
