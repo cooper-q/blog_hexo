262dfbf4bfd7c6322ef98a41da286feec4eee4ce
cooper-q <19221882+cooper-q@users.noreply.github.com>
2021-10-07 16:06:27 +0800

    feat(renew): update

--- shell/install_certificate.sh
+++ shell/install_certificate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-"$HOME/.acme.sh/acme.sh" --issue -d "www.mengxc.info" -d "blog.mengxc.info" --standalone -k ec-256
+sudo "$HOME/.acme.sh/acme.sh" --issue -d "www.mengxc.info" -d "blog.mengxc.info" --standalone -k ec-256
+#sudo "$HOME/.acme.sh/acme.sh" --issue -d "blog.mengxc.info" --standalone -k ec-256
 #~/.acme.sh/acme.sh --issue -d blog.mengxc.info --standalone -k ec-256
 
-"$HOME/.acme.sh/acme.sh" --installcert -d "www.mengxc.info" --fullchainpath /root/project/MattMeng_hexo/certificate/www.mengxc.info.crt --keypath /root/project/MattMeng_hexo/certificate/www.mengxc.info.key  --ecc
-"$HOME/.acme.sh/acme.sh" --installcert -d "blog.mengxc.info" --fullchainpath /root/project/MattMeng_hexo/certificate/www.mengxc.info.crt --keypath /root/project/MattMeng_hexo/certificate/www.mengxc.info.key  --ecc
+sudo "$HOME/.acme.sh/acme.sh" --installcert -d "blog.mengxc.info" --fullchainpath /root/project/MattMeng_hexo/certificate/blog.mengxc.info.crt --keypath /root/project/MattMeng_hexo/certificate/blog.mengxc.info.key  --ecc
+sudo "$HOME/.acme.sh/acme.sh" --installcert -d "www.mengxc.info"  --fullchainpath /root/project/MattMeng_hexo/certificate/www.mengxc.info.crt  --keypath /root/project/MattMeng_hexo/certificate/www.mengxc.info.key  --ecc
