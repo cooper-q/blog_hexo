49e54a5ee8724535c4001c692dd38d6776b1604a
cooper-q <19221882+cooper-q@users.noreply.github.com>
2023-01-11 17:34:59 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+version=`cat ./git_hook|awk -F "[.]" '{print $3}'`
+version=`expr ${version} + 1`
+echo v0.0.${version} > git_hook
+echo v0.0.${version}
+git commit -am '部署'
 ssh ma <./shell/reload.sh
 
 # 添加一个githook 并提交至githook服务器进行部署
